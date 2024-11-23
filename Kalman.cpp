62a887c23600afa9ca18601761114da1cf2343dd
eeeethan02 <eeeethan02@gmail.com>
2024-11-22 20:20:51 -0500

    Add files via upload

--- Kalman.cpp
+++ Kalman.cpp
@@ -27,17 +27,18 @@
 
 using namespace std;
 
-//constexpr float M_PI = 3.1415925F;
+// constexpr float M_PI = 3.1415925F;
 
-Kalman::Kalman() {
+Kalman::Kalman()
+{
     /* We will set the variables like so, these can also be tuned by the user */
     Q_angle = 0.001f; // f means it's a float and not a double to save memory
     Q_bias = 0.003f;
     R_measure = 0.03f;
 
-    angle[3] = 0.0f; // Reset the angle
-    bias = 0.0f; // Reset bias
-    rate = 0.0f; 
+    angle = 0.0f; // Reset the angle
+    bias = 0.0f;  // Reset bias
+    rate = 0.0f;
 
     P[0][0] = 0.0f; // Since we assume that the bias is 0 and we know the starting angle (use setAngle), the error covariance matrix is set like so - see: http://en.wikipedia.org/wiki/Kalman_filter#Example_application.2C_technical
     P[0][1] = 0.0f;
@@ -45,8 +46,22 @@ Kalman::Kalman() {
     P[1][1] = 0.0f;
 };
 
+Kalman::Kalman(float Qangle, float Qbias, float Rmeasure, float startAngle)
+{
+    Q_angle = Qangle;
+    Q_bias = Qbias;
+    R_measure = Rmeasure;
+
+    angle = startAngle;
+    P[0][0] = 0.0f;
+    P[0][1] = 0.0f;
+    P[1][0] = 0.0f;
+    P[1][1] = 0.0f;
+}
+
 // The angle should be in degrees and the rate should be in degrees per second and the delta time in seconds
-float Kalman::getAngle(int dir, float newAngle, float newRate, float dt){
+float Kalman::filterSync(float newAngle, float newRate, float dt)
+{
     // KasBot V2  -  Kalman filter module - http://www.x-firm.com/?page_id=145
     // Modified by Kristian Lauszus
     // See my blog post for more information: http://blog.tkjelectronics.dk/2012/09/a-practical-approach-to-kalman-filter-and-how-to-implement-it
@@ -55,12 +70,12 @@ float Kalman::getAngle(int dir, float newAngle, float newRate, float dt){
     // Update xhat - Project the state ahead
     /* Step 1 */
     rate = newRate - bias;
-    angle[dir] += dt * rate;
+    angle += dt * rate;
 
     // Update estimation error covariance - Project the error covariance ahead
     // Using 2d array to represent 2x2 matrix P (P[row][column])
     /* Step 2 */
-    P[0][0] += dt * (dt*P[1][1] - P[0][1] - P[1][0] + Q_angle);
+    P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
     P[0][1] -= dt * P[1][1];
     P[1][0] -= dt * P[1][1];
     P[1][1] += Q_bias * dt;
@@ -76,9 +91,9 @@ float Kalman::getAngle(int dir, float newAngle, float newRate, float dt){
 
     // Calculate angle and bias - Update estimate with measurement zk (newAngle)
     /* Step 3 */
-    float y = newAngle - angle[dir]; // Angle difference
+    float y = newAngle - angle; // Angle difference
     /* Step 6 */
-    angle[dir] += K[0] * y;
+    angle += K[0] * y;
     bias += K[1] * y;
 
     // Calculate estimation error covariance - Update the error covariance
@@ -91,12 +106,51 @@ float Kalman::getAngle(int dir, float newAngle, float newRate, float dt){
     P[1][0] -= K[1] * P00_temp;
     P[1][1] -= K[1] * P01_temp;
 
-    return angle[dir];
+    return angle;
 };
 
-void Kalman::setAngle(int dir, float angle) { 
-    this->angle[dir] = angle;
-    }; // Used to set angle, this should be set as the starting angle
+void Kalman::filterAsync(std::ifstream &gyroCSV, std::ifstream &accelCSV, std::ofstream &filteredCSV)
+{
+    if (!gyroCSV)
+    {
+        cout << "Gyro file not open" << endl;
+        return;
+    };
+    if (!accelCSV)
+    {
+        cout << "Accelerometer file not open" << endl;
+        return;
+    };
+    if (!filteredCSV)
+    {
+        cout << "Filtered file not open" << endl;
+        return;
+    };
+    float time = 0.0, temp = 0.0, noisyData = 0.0, cleanData = 0.0, filteredData = 0.0;
+    string line;
+    getline(gyroCSV, line);
+    getline(accelCSV, line);
+    cout << line << endl;
+    filteredCSV << "time, filtered sine wave" << endl;
+    while (gyroCSV >> time && accelCSV >> temp)
+    {
+        int comma = ',';
+        gyroCSV.ignore(1, comma);
+        accelCSV.ignore(1, comma);
+        gyroCSV >> noisyData;
+        accelCSV >> cleanData;
+        filteredData = this->filterSync(noisyData, cleanData, this->timeStep);
+        filteredCSV << time << ", " << filteredData << endl;
+    }
+    cout << "done";
+    return;
+}
+
+void Kalman::setAngle(float angle)
+{
+    this->angle = angle;
+}; // Used to set angle, this should be set as the starting angle
+
 float Kalman::getRate() { return this->rate; }; // Return the unbiased rate
 
 /* These are used to tune the Kalman filter */
@@ -104,91 +158,27 @@ void Kalman::setQangle(float Q_angle) { this->Q_angle = Q_angle; };
 void Kalman::setQbias(float Q_bias) { this->Q_bias = Q_bias; };
 void Kalman::setRmeasure(float R_measure) { this->R_measure = R_measure; };
 
-void Kalman::settimeStep(float time_step) {this->timeStep = time_step; };
+void Kalman::settimeStep(float time_step) { this->timeStep = time_step; };
 
 float Kalman::getQangle() { return this->Q_angle; };
 float Kalman::getQbias() { return this->Q_bias; };
 float Kalman::getRmeasure() { return this->R_measure; };
 
-float Kalman::gettimeStep() { return this->timeStep;};
+float Kalman::gettimeStep() { return this->timeStep; };
 
-float IMU::getGyro(char dir) { return 0 ;}; // Change later
-float IMU::getAccel(char dir) { return 0 ;}; // Change later
-
-void IMU::updateGyro(float &valueX, float &valueY, float &valueZ) {
-    valueX = 0; // Change later
-    valueY = 0; // Change later
-    valueZ = 0; // Change later
-};
-void IMU::updateAccel(float &valueX, float &valueY, float &valueZ) {
-    valueX = 0; // Change later
-    valueY = 0; // Change later
-    valueZ = 0; // Change later
-};
-
-void init(){
-    Kalman& filter1 = Kalman::get_instance();
-    IMU& imu1 = IMU::get_instance();
-
-    filter1.setQangle(0.001); // Change later
-    filter1.setQbias(0.003); // Change later
-    filter1.setRmeasure(0.03); // Change later
-
-    filter1.setAngle(IMU::X_AXIS, 0);
-    filter1.setAngle(IMU::Y_AXIS, 0);
-    filter1.setAngle(IMU::Z_AXIS, 0);
-
-    filter1.settimeStep(1); // Change later
-}
-
-void filterCSV(){
-    Kalman& filter1 = Kalman::get_instance();
-    cout << "Now reading files" << endl;
+void csvTest()
+{
+    Kalman filterX(0.001, 0.003, 0.03, 0.0);
     ifstream noiseReader("C:\\Users\\eeeet\\OneDrive\\Desktop\\UWRobotics\\Kalman Filter\\noisy_sine_wave.csv");
     ifstream cleanReader("C:\\Users\\eeeet\\OneDrive\\Desktop\\UWRobotics\\Kalman Filter\\clean_sine_wave.csv");
     ofstream writer("C:\\Users\\eeeet\\OneDrive\\Desktop\\UWRobotics\\Kalman Filter\\filtered_sine_wave.csv");
-    float time = 0.0, temp = 0.0, noisyData = 0.0, cleanData = 0.0, filteredData = 0.0;
-    if (!noiseReader.is_open()) {
-        cout << "Noise Read not open" << endl;
-    }
-    string line;
-    getline(noiseReader, line);
-    getline(cleanReader, line);
-    cout << line << endl;
-    writer << "time, filtered sine wave" << endl;
-    while (noiseReader >> time && cleanReader >> temp){
-        int comma = ',';
-        noiseReader.ignore(1, comma);
-        cleanReader.ignore(1, comma);
-        noiseReader >> noisyData;
-        cleanReader >> cleanData;
-        filteredData = filter1.getAngle(IMU::X_AXIS, noisyData, cleanData, filter1.gettimeStep());
-        cout << filteredData << endl;
-        writer << time << ", " << filteredData << endl;
-    }
-    cout << "done";
+    filterX.filterAsync(noiseReader, cleanReader, writer);
     writer.close();
+    return;
 }
 
-void action() {
-    float gyroX = 0, gyroY = 0, gyroZ = 0, accelX = 0, accelY = 0, accelZ = 0;
-    while (true) {
-        IMU& imu1 = IMU::get_instance();
-        imu1.updateGyro(gyroX, gyroY, gyroZ);
-        imu1.updateAccel(accelX, accelY, accelZ);
-        Kalman& filter1 = Kalman::get_instance();
-        float gyroX = filter1.getAngle(IMU::X_AXIS, gyroX, atan2(accelY, accelZ), filter1.gettimeStep());
-        float gyroY = filter1.getAngle(IMU::Y_AXIS, gyroX, atan2(accelX, accelZ), filter1.gettimeStep());
-        float gyroZ = filter1.getAngle(IMU::Z_AXIS, gyroX, atan2(accelX, accelY), filter1.gettimeStep());
-        this_thread::sleep_for(chrono::seconds(1));
-        // send data somewhere
-    };
-}
-
-int main(){
-    init();
-    filterCSV();
-    action();
-    
+int main()
+{
+    csvTest();
     return 1;
 }
--- Kalman.h
+++ Kalman.h
@@ -18,33 +18,28 @@
 #ifndef _Kalman_h_
 #define _Kalman_h_
 
-class Kalman {
-protected:
-    Kalman(); // Ensures default constructor is used by compiler
-public:
-    static Kalman& get_instance(){
-        static Kalman instance; // Static ensures that the same instance is used every time
-        return instance;
-    }
-    
-    // Making copy, move constructors deleted functions
-    Kalman(const Kalman&) = delete;
-    Kalman(Kalman&&) = delete;
+#include <iostream>
+#include <fstream>
 
-    // Making copy, move assignment operators deleted functions
-    Kalman& operator = (const Kalman&) = delete;
-    Kalman& operator=(Kalman&&) = delete;
+class Kalman
+{
+public:
+    Kalman();
+    Kalman(float Qangle, float Qbias, float Rmeasure, float startAngle);
 
     // The angle should be in degrees and the rate should be in degrees per second and the delta time in seconds
-    float getAngle(int dir, float newAngle, float newRate, float dt);
-    void setAngle(int dir, float angle); // Used to set angle, this should be set as the starting angle
-    float getRate(); // Return the unbiased rate
+    float filterSync(float newAngle, float newRate, float dt);
+    void filterAsync(std::ifstream& gyroCSV, std::ifstream& accelCSV, std::ofstream& filteredCSV);
+
+protected:
+    void setAngle(float startAngle); // Used to set angle, this should be set as the starting angle
+    float getRate();                 // Return the unbiased rate
 
     /* These are used to tune the Kalman filter */
     void setQangle(float Q_angle);
     /**
      * setQbias(float Q_bias)
-     * Default value (0.003f) is in Kalman.cpp. 
+     * Default value (0.003f) is in Kalman.cpp.
      * Raise this to follow input more closely,
      * lower this to smooth result of kalman filter.
      */
@@ -61,61 +56,16 @@ public:
 
 private:
     /* Kalman filter variables */
-    float Q_angle; // Process noise variance for the accelerometer
-    float Q_bias; // Process noise variance for the gyro bias
+    float Q_angle;   // Process noise variance for the accelerometer
+    float Q_bias;    // Process noise variance for the gyro bias
     float R_measure; // Measurement noise variance - this is actually the variance of the measurement noise
 
-    float angle[3];
+    float angle;
     float bias; // The gyro bias calculated by the Kalman filter - part of the 2x1 state vector
     float rate; // Unbiased rate calculated from the rate and the calculated bias - you have to call getAngle to update the rate
     float timeStep;
 
     float P[2][2]; // Error covariance matrix - This is a 2x2 matrix
 };
-
-class IMU {
-protected:
-    IMU() = default;
-public:
-    static IMU& get_instance(){
-        static IMU instance;
-        return instance;
-    };
-
-        // Making copy, move constructors deleted functions
-    IMU(const IMU&) = delete;
-    IMU(Kalman&&) = delete;
-
-    // Making copy, move assignment operators deleted functions
-    IMU& operator = (const IMU&) = delete;
-    IMU& operator=(IMU&&) = delete;
-
-    static constexpr float X_AXIS = 0.0f;
-    static constexpr float Y_AXIS = 1.0f;
-    static constexpr float Z_AXIS = 2.0f;
-
-    void updateGyro(float &valueX, float &valueY, float &valueZ); // Add later
-    void updateAccel(float &valueX, float &valueY, float &valueZ); // Add later
-
-    float getGyro(char dir);
-    float getAccel(char dir);
-private:
-    float gyroX;
-    float gyroY;
-    float gyroZ;
-    float accelX;
-    float accelY;
-    float accelZ;
-};
-
-struct Point {
-    float data;
-    Point* next;
-};
-typedef Point *PointPtr;
-
-void init();
-void filterCSV();
-void action();
 
 #endif
